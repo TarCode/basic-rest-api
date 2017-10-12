ba51a3952c99b56e63dc96a3cede142f6f1dbbcf
TarCode <tarcode33@gmail.com>
2017-10-12 10:58:01 +0200

    added get and post routes and added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+
+# What's a REST API?
+Initially, the World Wide Web was created for documents (HTML). Today, there's a name for every type of entity that can be identified.
--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+})
+
+app.post('/person', (req, res) => {
+    const data = req.body;
+
+    console.log("This is the person, ", data);
+})
+
 app.listen(3000, () => {
     console.log("App listening on port 3000");
 })
